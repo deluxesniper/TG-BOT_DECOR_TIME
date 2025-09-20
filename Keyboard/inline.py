67f43a28e598d63e6b8be7d201d8bce52169ad43
deluxesniper <deluxesniper@gmail.com>
2025-09-20 14:09:10 +0300

    20/09/25_12_14_08

--- Keyboard/inline.py
+++ Keyboard/inline.py
@@ -1,10 +1,12 @@
+from gc import callbacks
+
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def info_inline():
     kb_list=[
-        [InlineKeyboardButton(text='Информация о нас',callback_data='button1'),
-        InlineKeyboardButton(text="Наш сайт", url='https://decortime.pro/')]
-        [InlineKeyboardButton(text="Расчет кол-во Штукатурки, Лака и Грунтовки")]
+        [InlineKeyboardButton(text='Информация о нас',callback_data='info'),
+        InlineKeyboardButton(text="Наш сайт", url='https://decortime.pro/')],
+        [InlineKeyboardButton(text="Расчет кол-во Штукатурки, Лака и Грунтовки", callback_data='Raschet')],
     ]
     keyboard=InlineKeyboardMarkup(inline_keyboard=kb_list)
     return keyboard
--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
 from logs.logs import logging
 from configs.config import TOKEN
-from Keyboard.inline import info_inline
-
-dp=Dispatcher()
-bot=Bot(token=TOKEN)
+from handlers import Router, router
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    dp = Dispatcher()
+    bot = Bot(token=TOKEN)
+    dp.include_router(router)
     await dp.start_polling(bot)
-
-
-@dp.message(Command('start'))
-async def start(message: Message):
-    await message.answer(f"""Здравствуйте! {message.from_user.full_name} \nВас приветствует компания ООО «ДекорТайм»\nОфициальный дистрибьютор производителей декоративных покрытий:
-Cebos Color, Valpaint, San Marco, Ticiana Deluxe, Werth Deco и представитель лакокрасочной продукции Flugger.\n\nМожете ознакомиться с моими функциями ниже""", reply_markup=info_inline())
-
 
 
 
