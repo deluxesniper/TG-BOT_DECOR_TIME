0c2b60d7997e603409cb9c4367d20519f2064f8c
deluxesniper <deluxesniper@gmail.com>
2025-09-21 08:27:55 +0300

    21_09_25_08_27_00

---
+++ stor/store.py
@@ -0,0 +1,4 @@
+from stor.contacts import Twinstor_email, Twinstor_call, Twinstor_time, Twinstor_locations
+Twinstor=(f"Адрес: {Twinstor_locations} время работы: {Twinstor_time} телефон: {Twinstor_call} email@: {Twinstor_email} ")
+
+print(Twinstor)
